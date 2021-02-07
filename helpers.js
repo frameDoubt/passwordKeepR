b151a8a8281a0ce501249c39b58228c21c2a99c8
Jacob Allen <jallen2033@gmail.com>
2021-02-07 06:54:23 +0000

    added error handling for empty input on the email and password forms at login screen

--- helpers.js
+++ helpers.js
@@ -12,28 +12,19 @@ const pool = new Pool({
   database: 'midterm'
 });
 
-// helper function to check if a users email address already exists in our database
-// Fixed!
-const emailExists = function(userEmail) {
+// helper function to check if a users email address already exists in our database - FIXED!
+const emailExists = function (userEmail) {
 
   if (userEmail) {
-    console.log("\n userEmail in function: ", userEmail)
-
     const query = `
        SELECT email
        FROM users 
        WHERE email = '${userEmail}';
       `
-  
-    console.log("\n query: ", query);
-      // const values = [userEmail]
-  
-      return pool.query(query)
+    return pool.query(query)
       .then(res => {
-        // console.log("\n res.rows[0] email: ", res.rows[0].email);
-  
+
         if (res.rows[0].email === userEmail) {
-          // console.log("res.rows[0] insife if: ", res.rows[0]);
           return true;
         } else {
           return false;
@@ -43,38 +34,26 @@ const emailExists = function(userEmail) {
   return false;
 };
 
-// helper function to check if a users id already exists in our database
-const isAuthenticated = function(userid, users) {
-  let returnBool = false;
- 
+// helper function that takes in req.body.email as "userEmail" and the users object, added hashing! much security! - WORKS!
+const passwordValidator = function (userPassword, userEmail) {
 
- 
-  return returnBool;
-};
-
-/* helper function that takes in req.body.email as "userEmail" and the users object, added hashing! much security!
-    WORKS! */
-   const passwordValidator = function(userPassword, userEmail) {
-    
-    const query = `
+  const query = `
      SELECT id, master_password 
      FROM users 
      WHERE email = '${userEmail}';
     `
-    console.log("query", query);
+  console.log("query", query);
 
-    return pool.query(query)
+  return pool.query(query)
     .then(res => {
-      // console.log("res.rows[0] outside if statement", res.rows[0]);
 
       if (res.rows[0].master_password === userPassword) {
-        // console.log("res.rows[0] in if statement", res.rows[0]);
         return res.rows[0].id;
       } else {
         return false;
       }
     });
-  };
+};
 
 // export these helper functions to where they are needed
-module.exports = { emailExists, isAuthenticated, passwordValidator};
+module.exports = { emailExists, passwordValidator };
--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const loginRoute = express.Router();
-const { emailExists, isAuthenticated, passwordValidator } = require("../helpers.js");
+const { emailExists, passwordValidator } = require("../helpers.js");
 
-// require and use cookie session to store user ids for cookie sessions
-// https://www.npmjs.com/package/cookie-session
+/* require and use cookie session to store user ids for cookie sessions
+ * https://www.npmjs.com/package/cookie-session */
 const cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: 'user_id',
@@ -13,14 +13,14 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000
 }));
 
-// GET route
-// https://stackoverflow.com/questions/29941208/express-error-typeerror-router-use-requires-middleware-function-but-got-a-o
+/* GET route
+ * https://stackoverflow.com/questions/29941208/express-error-typeerror-router-use-requires-middleware-function-but-got-a-o */
 loginRoute.get("/", (req, res) => {
   res.render("login");
 });
 
-// POST route
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
+/* POST route
+   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then */
 loginRoute.post("/", (req, res) => {
   const { email, password } = req.body;
   console.log("email from user input: ", email);
@@ -31,24 +31,30 @@ loginRoute.post("/", (req, res) => {
     invalidPassword: "Invalid Password!"
   };
 
-  // promise chain that will first check if the users email exists against our db and then validate their password
+  // syncronous error handling before our promise chain below even starts 
+  if (!email) {
+    const templateVars = {
+      error: errors.email
+    };
+    res.status(400).render('error', templateVars);
+  } else if (!password) {
+    const templateVars = {
+      error: errors.password
+    };
+    res.status(400).render('error', templateVars);
+  }
+
+  /* promise chain that will first check if the users email exists against our db and then validate their password
+   * async error handling for when a username or password is invalid will happen in here - TODO */
   let validUserEmail = emailExists(email);
   validUserEmail.then((value) => {
     return passwordValidator(password, email);
-  }).then((value => {
+  }).then((value) => {
     req.session = value;
     res.redirect('/');
-  }));
+  }).catch(error => {
+    console.log(error)
+  });
 });
 
-
 module.exports = loginRoute;
-
-
-// isAuthenticated.then((value) => {
-//   console.log("\n value inside of isAuthenticated returned promise: ", value);
-
-//   console.log("isAuthenticated we want YEE: ", value);
-//   req.session = value;
-//   res.redirect('/');
-// });
