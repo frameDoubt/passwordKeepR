b9475081dc7163c584fa22d42115113e1061c209
Paxon Cheung <paxon.cheung@gmail.com>
2021-02-07 03:34:44 +0000

    adding fake data

---
+++ db/seeds/03_passwords.sql
@@ -0,0 +1,15 @@
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (1, 1, 'social', 'www.facebook.com', '9873masdsa7', 'Facebook');
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (1, 1, 'social', 'www.reddit.com', 'dlksaj87s', 'Reddit');
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (1, 2, 'work related', 'www.worklogin.com', '9873masdsa7', 'Worklogin');
+
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (2, 3, 'entertainment', 'www.youtube.com', 'fkjashfjaks8', 'Youtube');
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (2, 3, 'social', 'www.reddit.com', 'fasfhkashfkj', 'Reddit');
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (2, 3, 'work related', 'www.worklogin.com', '9873masdsa7', 'Worklogin');
+
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (3, 3, 'social', 'www.instagram.com', 'kljfasj', 'Instagram');
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (3, 3, 'social', 'www.twitter.com', 'dlsadasdfa', 'Twitter');
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (3, 3, 'work related', 'www.worklogin3.com', '9fasfs', 'Worklogin3');
+
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (4, NULL, 'entertainment', 'www.disneyplus.com', 'fsafasfsafa', 'Disney');
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (4, NULL, 'social', 'www.twitter.com', 'uiha78s68aj', 'Twitter');
+INSERT INTO passwords (user_id, organisations_id, category, url, password_text, title) VALUES (4, NULL, 'entertainment', 'www.netflix.com', 'lhsafua', 'Netflix');
--- db/seeds/04_users_organisations.sql
+++ db/seeds/04_users_organisations.sql
@@ -1,15 +1,6 @@
 INSERT INTO users_organisations (user_id, organisations_id) VALUES (1, 1);
 INSERT INTO users_organisations (user_id, organisations_id) VALUES (1, 2);
 INSERT INTO users_organisations (user_id, organisations_id) VALUES (2, 3);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (3, 4);
+INSERT INTO users_organisations (user_id, organisations_id) VALUES (3, 3);
 INSERT INTO users_organisations (user_id, organisations_id) VALUES (4, NULL);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (5, 5);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (6, 5);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (7, 1);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (8, 1);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (9, 2);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (10, 3);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (11, 4);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (12, 5);
-INSERT INTO users_organisations (user_id, organisations_id) VALUES (13, 1);
 
