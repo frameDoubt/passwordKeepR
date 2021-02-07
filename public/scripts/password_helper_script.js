bcaf97b2d7a9ef5a3201635e61d660030da6fa86
Brandon Wright <brandon.w.wright4@gmail.com>
2021-02-07 05:43:59 +0000

    add helper function to make passwords not complete yet

---
+++ public/scripts/password_helper_script.js
@@ -0,0 +1,18 @@
+const generateRandomString = function(length, options) {
+  const passwordBuilder = function() {
+    for (i = 0; i < length; i++) {
+      retStr += alphNumArr[Math.floor(Math.random() * alphNumArr.length)];
+    };
+  }
+  let lwrAlphaArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let upprAlphaArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let symbolArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "?"];
+  let numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let retStr = "";
+  switch (options) {
+    case 'lusn':
+      
+
+  }
+  return retStr;
+};
