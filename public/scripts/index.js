ab91b2f8491d4eccc3236f06d304da0acff50bd9
Jacob Allen <jallen2033@gmail.com>
2021-02-09 21:47:14 +0000

    added front end logic for handling password edit requests

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   console.log("Hello world");
 
+  // variables to store the elements we want to manipulate
   let del_button = document.getElementsByClassName("remove_from_password_buttons");
   let del_button2 = document.getElementsByClassName("remove_from_password_buttons_company");
   let edit_button = document.getElementsByClassName("edit_password");
@@ -9,10 +10,7 @@ $(document).ready(function () {
   let cancel_button2 = document.getElementsByClassName("cancel_edits_company");
   let submit_button = document.getElementsByClassName("submit_edits");
   let submit_button2 = document.getElementsByClassName("submit_edits_company");
-
-  console.log("del_button:", del_button);
-
-  console.log(del_button.length)
+  let cachedPassword;
 
   // attaches listeners to my passwords - to delete
   for (let i = 0; i < del_button.length; i++) {
@@ -48,54 +46,58 @@ $(document).ready(function () {
 
   // attaches listeners to personal passwords - to edit - TODO
   for (let i = 0; i < edit_button.length; i++) {
-    edit_button[i].onclick = function () {
+    edit_button[i].onclick = function (event) {
+      button_id = edit_button[i].id;
+      cachedPassword = $(event.target).parent().parent().find('.password_to_edit').val();
       edit_button_id = edit_button[i].id;
-      console.log("button_id1: ", edit_button_id);
       edit_button[i].style.display = "none";
       cancel_button[i].style.display = "block";
       submit_button[i].style.display = "block";
+      $(event.target).parent().parent().find('.password_to_edit').prop('disabled', false);
 
-      // if (submit button clicked on) {
-      //   $.ajax({ method: 'POST', url: '/editPassword', data: { clicked_button: button_id } })
-      //   .then(function (response) {
-      //     // $(`div.personal_passwords[data-id=${response}]`)[0].style.display = "none";
-      //     // $(`div.company_passwords[data-id=${response}]`)[0].style.display = "none";
-      //     console.log("response from server with id test: ", response);
-      //   });
-      // }
+      submit_button[i].onclick = function () {
+        $.ajax({ method: 'POST', url: '/editPassword', data: { clicked_button: button_id } })
+        .then(function (response) {
+          // $(`div.personal_passwords[data-id=${response}]`)[0].style.display = "none";
+          // $(`div.company_passwords[data-id=${response}]`)[0].style.display = "none";
+          console.log("response from server with id test: ", response);
+        });
+      }
 
       cancel_button[i].onclick = function () {
+        $(event.target).parent().parent().find('.password_to_edit').val(cachedPassword);
         edit_button[i].style.display = "block";
         cancel_button[i].style.display = "none";
         submit_button[i].style.display = "none";
+        $(event.target).parent().parent().find('.password_to_edit').prop('disabled', true);
       }
     }
    }
 
   // attaches listeners to company passwords - to edit - TODO
   for (let i = 0; i < edit_button2.length; i++) {
-    edit_button2[i].onclick = function () {
+    edit_button2[i].onclick = function (event) {
       button_id = edit_button2[i].id;
-      console.log("button_id2: ", button_id);
       edit_button2[i].style.display = "none";
       cancel_button2[i].style.display = "block";
       submit_button2[i].style.display = "block";
+      $(event.target).parent().parent().find('.password_to_edit').prop('disabled', false);
 
-      // if (submit button clicked on) {
-      //   $.ajax({ method: 'POST', url: '/editPassword', data: { clicked_button: button_id } })
-      //   .then(function (response) {
-      //     // $(`div.personal_passwords[data-id=${response}]`)[0].style.display = "none";
-      //     // $(`div.company_passwords[data-id=${response}]`)[0].style.display = "none";
-      //     console.log("response from server with id test: ", response);
-      //   });
-      // }
+      submit_button[i].onclick = function () {
+        $.ajax({ method: 'POST', url: '/editPassword', data: { clicked_button: button_id } })
+        .then(function (response) {
+          // $(`div.personal_passwords[data-id=${response}]`)[0].style.display = "none";
+          // $(`div.company_passwords[data-id=${response}]`)[0].style.display = "none";
+          console.log("response from server with id test: ", response);
+        });
+      }
 
       cancel_button2[i].onclick = function () {
         edit_button2[i].style.display = "block";
         cancel_button2[i].style.display = "none";
         submit_button2[i].style.display = "none";
+        $(event.target).parent().parent().find('.password_to_edit').prop('disabled', true);
       }
     }
   }
 });
-
