076257c16c56b7f6eb51608311ac4f114a712118
Brandon Wright <brandon.w.wright4@gmail.com>
2021-02-10 00:33:39 +0000

    add helper function that displays range sliders value in password_gen.ejs template

--- public/scripts/password_helper_script.js
+++ public/scripts/password_helper_script.js
@@ -1,25 +1,9 @@
-// const generateRandomString = function(length, options) {
-//   const passwordBuilder = function() {
-//     for (i = 0; i < length; i++) {
-//       retStr += alphNumArr[Math.floor(Math.random() * alphNumArr.length)];
-//     };
-//   }
-//   let lwrAlphaArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//   let upprAlphaArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//   let symbolArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "?"];
-//   let numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-//   let retStr = "";
-//   switch (options) {
-//     case 'lusn':
-
-
-//   }
-//   return retStr;
-// };
 
 // function update counter for password length range input
 $(document).ready(function() {
-  $('#pwordLenRange').on('input', function() {
-    console.log(this);
+  console.log('hello world');
+  $('#passwordLength').on('input', function() {
+    console.log('Real-Time Value:', this.value);
+    $('#slider_value').html(this.value);
   });
 });
