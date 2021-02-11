ab66908d540cc95ee18830774ef79920f0eb1a9c
Paxon Cheung <paxon.cheung@gmail.com>
2021-02-11 02:13:42 +0000

    adding second form to type own password

--- public/scripts/password_helper_script.js
+++ public/scripts/password_helper_script.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
   });
   $('#pwordSubmission').on('click', function() {
     const url = $('#urlInput').val();
-    const password = 'password';
     const passwordLength = $('#passwordLength').val();
     const upperCaseVal = document.querySelector('#upperCaseCheck').checked;
     const lowerCaseVal = document.querySelector('#lowerCaseCheck').checked;
@@ -18,11 +17,6 @@ $(document).ready(function() {
     const symbolVal = document.querySelector('#symbolsCheck').checked;
     const organisationName = document.querySelector('#orgName').value;
     const category = document.querySelector('#catName').value;
-    console.log('category pull: ', category)
-    console.log('organisation name pull: ', organisationName)
-    console.log(typeof organisationName)
-
-
 
     // to add to pass through AJAX post method: USERID, ORGANISATIONID OR NAME, CATEGORY
     $.ajax({ method: 'POST', url: '/password_gen/', data: {
@@ -33,7 +27,24 @@ $(document).ready(function() {
       numbers: numberCheckVal,
       symbols: symbolVal,
       organisationName: organisationName,
-      category: category
+      category: category,
+    } })
+    .then(function (response) {
+      console.log("response from the server is: ", response);
+    });
+  });
+  $('#pwordSubmission2').on('click', function() {
+    const url = $('#urlInput2').val();
+    const password = document.querySelector('#passwordField2').value;
+    const organisationName = document.querySelector('#orgName2').value;
+    const category = document.querySelector('#catName2').value;
+    console.log('these are sub 2 inputs: ', url, password, organisationName, category)
+
+    $.ajax({ method: 'POST', url: '/password_gen/', data: {
+      url: url,
+      organisationName: organisationName,
+      category: category,
+      password: password
     } })
     .then(function (response) {
       console.log("response from the server is: ", response);
